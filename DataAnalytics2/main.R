46a95e5a9e9c86a50c149c9bfc6d5bd041baeaf5
DESKTOP-13AG486\Christian <christian.wnr@gmail.com>
2018-06-25 21:23:31 +0200

    Data retrieval finished. How-To included

--- DataAnalytics2/main.R
+++ DataAnalytics2/main.R
@@ -9,14 +9,18 @@ require('stringr');
 
 url = 'http://optim.uni-muenster.de:5000/';
 operation = 'api-test2D/'
-data = '0.4,0.1;0.06,0.2;0.07,0.2' #you are providing one or more points here
+#data: Provide one or more points
+x <- c(0.4,0.06,0.07,0.09);
+y <- c(0.1,0.2,0.2,0.01);
+data <- cbind(x,y);
 test = TRUE;
+#token
 token = '5d8096530da349e98ca4cc65b519daf7';
 
-#run getRequest() and insert the provided token to do the GET request. Outputs an xml2-object.
-getRequest<- function(d, token){
+#Executes the GET request. Outputs an xml2-object.
+getRequest<- function(data, token){
   #token <- readline(prompt="Token: ")
-  getUrl <- paste(url,operation,token,'/',d, sep="");
+  getUrl <- paste(url,operation,token,'/',data, sep="");
   if(grepl('test',getUrl) || (!test)){
     r <- GET(getUrl);
     return (r);
@@ -26,16 +30,27 @@ getRequest<- function(d, token){
   }
 };
 
-#Turns the response object of the get-request into a data frame
-getData<- function(d, token){
-  request <- getRequest(d, token);
-  #TODO: Figure out how to turn an xml2-object into a data frame
+#Receives dataframe as input, turns it into a string, calls getRequest(), turns response object back into numeric and outputs a dataframe.
+#remark: run print(getData(data, token), digits=20) to see that the result is NOT rounded
+getData<- function(data, token){
+  dataAsString <- '';
+  for(row in 1:nrow(data)){
+    for(col in 1:ncol(data)){
+      dataAsString <- paste(dataAsString, as.character(data[row,col]), sep='');
+      if(col < ncol(data)){
+        dataAsString <- paste(dataAsString, ",", sep='');
+      }
+    }
+    if(row < nrow(data)){
+      dataAsString <- paste(dataAsString, ";", sep='');
+    }
+  }
+  request <- getRequest(dataAsString, token);
   html <- content(request, "text");
   substring <- sub(".*\\[", "", html);
   substring <- sub("\\].*", "", substring);
   substring <- str_extract_all(substring, "[0-9].[0-9]*", simplify = TRUE)
-  return (as.numeric(substring));
-  #remark: run print(getData(data, token), digits=20) to see that the result is NOT rounded
+  return (cbind(data,r=as.numeric(substring)));
 }
 
 #execute: getData(data, token)
