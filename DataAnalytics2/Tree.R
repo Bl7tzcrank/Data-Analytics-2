1ef307d1638d3dad3e31e237be1aa90d5d05b199
CL <chad__lee@hotmail.com>
2018-07-07 00:53:11 +0200

    Working basics 🌳 take 2

--- DataAnalytics2/Tree.R
+++ DataAnalytics2/Tree.R
@@ -1,10 +1,38 @@
-install.packages("rpart");
-install.packages("xgboost");
+install.packages('rpart');
+install.packages('randomForest');
+install.packages('xgboost');
+install.packages('party');
 require('rpart');
+require('randomForest');
 require('xgboost');
+source("main.R")
 
-typeColNum <- grep("Class",names(train))
-#this is only for classification trees: rpart_model <- rpart(class~.,data=train,method="class") 
-## rpart_model <- rpart(col1~.,data = train, method="anova")
-rpart_model1 <- rpart(comp~col1 + col2, data=train, method="anova")
-plot(rpart_model1);text(rpart_model1)
+##data=train for training
+createTreeModel = function(dataset){
+  return(rpart_model <- rpart(r ~ col1 + col2, data=dataset, method="anova"))
+} 
+
+##Plot the full tree
+##plot(rpart_model);text(rpart_model);
+plot(rpart_model, uniform = TRUE, main="Decision Tree for train");
+text(rpart_model, use.n = TRUE, all=TRUE, cex=.8);
+
+##Cost-complexity pruning
+printcp(rpart_model);
+plotcp(rpart_model);
+summary(rpart_model);
+opt <- which.min(rpart_model$cptable[,"xerror"])
+cp <- rpart_model$cptable[opt, "CP"]
+pruned_model<-prune(rpart_model,cp)
+
+##output predictions as vector - 1 prediction per record in dataset
+test_predict <- predict(rpart_model, test, type = "vector");
+
+##calculate root-mean-square error by comparing test_predict vector with test
+rmsqe <- sqrt(mean((test_predict-test)^2))
+
+##Function to do multiple runs 
+##not sure if it makes sense to do this on train data, the rmsqe just reduces by 0.01
+
+
+## RF <- randomForest(r ~ col1 + col2, train, ntree=50)
